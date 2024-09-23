712a72f66362ae486f8eea2e2f204ea8cc16238d
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-09-23 21:02:51 +0800

    LO1-declaring string arrays

---
+++ src/desalan_it20/Desalan_IT20.java
@@ -0,0 +1,11 @@
+package desalan_it20;
+
+public class Desalan_IT20 {
+
+    public static void main(String[] args) {
+        //Declaring String Array
+        String[] name = {"Ahrdy", "Jane", "Desalan"};
+
+    }
+
+}
