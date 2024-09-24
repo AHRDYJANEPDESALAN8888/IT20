6a4824da36de8fcb9645b63350513dc44b3d449f
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-09-24 10:37:04 +0800

    LO2-multidimensional array

---
+++ src/LO2_MultidimensionalArrays/MultidimensionalArray.java
@@ -0,0 +1,13 @@
+package LO2_MultidimensionalArrays;
+
+public class MultidimensionalArray {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+    }
+
+}
