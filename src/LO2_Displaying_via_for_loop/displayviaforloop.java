adf604437493b94e5c80926a3a35a8ebc93fa666
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-09-24 10:28:27 +0800

    LO2-declaring string array

---
+++ src/LO2_Displaying_via_for_loop/displayviaforloop.java
@@ -0,0 +1,12 @@
+package LO2_Displaying_via_for_loop;
+
+public class displayviaforloop {
+
+    public static void main(String[] args) {
+
+        //Declaaring string arrays
+        String[] Names = {"Ahrdy", "Violet", "Purple"};
+
+    }
+
+}
