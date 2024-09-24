607e807e6cdaf94e649ffa8f32f3f630b42a8b6d
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-09-24 10:38:13 +0800

    LO2- displaying multidimensin arrays

--- src/LO2_MultidimensionalArrays/MultidimensionalArray.java
+++ src/LO2_MultidimensionalArrays/MultidimensionalArray.java
@@ -8,6 +8,14 @@ public class MultidimensionalArray {
             {4, 5, 6},
             {7, 8, 9}
         };
+        // Printing the 2D array
+        System.out.println("Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
     }
 
 }
