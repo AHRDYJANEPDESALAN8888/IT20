3603965e498c9078c995a2de992e02d7443143dd
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-09-27 10:21:33 +0800

    initial commit

---
+++ src/ARRAYLIST/ITEM_MANAGER.java
@@ -0,0 +1,9 @@
+package ARRAYLIST;
+
+public class ITEM_MANAGER {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/ARRAYLIST/MAIN.java
@@ -0,0 +1,9 @@
+package ARRAYLIST;
+
+public class MAIN {
+
+    public static void main(String[] args) {
+
+    }
+
+}
