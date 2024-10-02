4ccaafeb1b918009694c55f4610e3b10f2b4231e
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-02 21:14:53 +0800

    added method to print the linked list

--- src/topic_2_linkedlist_int/Main.java
+++ src/topic_2_linkedlist_int/Main.java
@@ -3,15 +3,18 @@ package topic_2_linkedlist_int;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        Linkedlist list = new Linkedlist();
 
         //adding elements to the list
         list.add(10);
         list.add(20);
         list.add(30);
         list.add(40);
+        list.add(50);
         System.out.println("Current Linked List:");
         list.printList();
+        
+      
 
     }
 
