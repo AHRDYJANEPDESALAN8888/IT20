b53234f4d756a02f5f9c4ccf0d64bf8c894be865
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-11 22:47:00 +0800

    Added code in Main class

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -1,21 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_3_stack;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the stack: ");
+        int size = scanner.nextInt();
+
+        Stack stack = new Stack(size);
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Push an integer");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Size");
+            System.out.println("5. Exit");
+            System.out.print("Choose an operation: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter an integer to push: ");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case 2:
+                    System.out.println("Popped element: " + stack.pop());
+                    break;
+                case 3:
+                    System.out.println("Top element is: " + stack.peek());
+                    break;
+                case 4:
+                    System.out.println("Stack size is: " + stack.size());
+                    break;
+                case 5:
+                    System.out.println("Exiting program.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
-    
 }
