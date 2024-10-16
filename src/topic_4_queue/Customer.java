d23aae1e1371e1c9b48cd81df3ab7b043c857063
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-16 16:27:17 +0800

    Added codes in queue class

--- src/topic_4_queue/Customer.java
+++ src/topic_4_queue/Customer.java
@@ -10,5 +10,14 @@ package topic_4_queue;
  * @author HP
  */
 public class Customer {
-    
+
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
