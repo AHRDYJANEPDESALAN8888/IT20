069bcbf2038efeff2fee523d2cde884c8aaf72d5
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-16 16:38:27 +0800

    Added switch case for operations

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -24,6 +24,39 @@ public class Main {
 
         System.out.println("Welcome to Simple Queue Program!");
         System.out.println();
+
+        while (true) {
+            System.out.println("Choose an Option to Perform"
+                    + "\n1. Add Customer"
+                    + "\n2. Serve Customer"
+                    + "\n3. Display Queue"
+                    + "\n4. Exit");
+
+            System.out.println();
+
+            System.out.print("Choice: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Customer name: ");
+                    String name = sc.next();
+                    q.enqueue(new Customer(name));
+                    break;
+                case 2:
+                    q.dequeue();
+                    break;
+                case 3:
+                    q.displayQueue();
+                    break;
+                case 4:
+                    System.out.println("Thannk You for running this simple program!");
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try Again!");
+            }
+
+        }
     }
 
 }
