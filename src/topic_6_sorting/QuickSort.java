f5a8b2f43a06b569000be77131503209e9afe7ce
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-11-28 22:44:43 +0800

    input code for quicksort3

--- src/topic_6_sorting/QuickSort.java
+++ src/topic_6_sorting/QuickSort.java
@@ -41,6 +41,12 @@ public class QuickSort {
 
         return i + 1;
     }
+     public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
 
