32cfa052d5617b1c4d6562c6a083e6a4e2eee416
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-11-28 22:31:56 +0800

    added insertsort

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -5,13 +5,29 @@
  */
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
  */
 public class InsertionSort {
-    
-    public static void main (String []args) {
-        
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
